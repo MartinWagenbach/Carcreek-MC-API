08c781806481bb14d83d407102b625e82ff11a4b
Martin Wagenbach <mimnu98@gmail.com>
2025-07-01 15:47:25 +0200

    Correcting auto caps

--- src/main/java/de/carcreek/CarcreekMcApi.java
+++ src/main/java/de/carcreek/CarcreekMcApi.java
@@ -5,8 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CarcreekMcApi extends JavaPlugin {
 
     private static CarcreekMcApi INSTANCE;
-    
-    public CarcreekMcApi getINSTANCE() {
+
+    public static CarcreekMcApi getInstance() {
         return INSTANCE;
     }
 
